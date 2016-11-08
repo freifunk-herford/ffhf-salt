3533de5501feb8bc090da27d66ade3830fb96d2d
sdovnic <sdovnic@web.de>
2016-11-08 04:03:30 +0100

    Recover Alfred from broken fastd Tunnel

--- file_roots/gateway/root/scripts/check-alfred.sh
+++ file_roots/gateway/root/scripts/check-alfred.sh
@@ -9,10 +9,14 @@ SHELL=/bin/bash
 
 if [ -f /bin/systemctl ]; then
 	if [ "$(systemctl status alfred | grep -E 'Active: inactive|Active: failed')" ]; then
+		systemctl restart fastd
+		sleep 3
 		systemctl restart alfred
 	fi
 else
 	if [ "$(service alfred status | grep stop)" ]; then
+		service fastd restart
+		sleep 3
 		service alfred restart
 	fi
 fi
