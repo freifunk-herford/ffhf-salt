9d86fd0f47a0964d337730bfddb14752811a20be
Salt Master Freifunk Herford <info@herford.freifunk.net>
2020-07-22 01:38:34 +0200

    Fix Bridge.

---
+++ file_roots/gateway/root/scripts/check-bridge.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Copyright 2020 - 2020 Tobias Benzin tbenzin@digital-nerv.net
+#                       Rally Vincent rvincent@digital-nerv.net
+
+# This file is managed by Salt, do not edit.
+
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+SHELL=/bin/bash
+
+batmanif="{{ pillar['network']['bridge']['interface'] }}"
+bridgeif="{{ pillar['network']['batman']['interface'] }}"
+
+if [[ ! $(brtctl show | grep ${batmanif}) ]]; then
+	brctl addif ${batmanif} ${bridgeif}
+fi
